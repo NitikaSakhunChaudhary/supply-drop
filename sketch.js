6b236039f388fb2d12b8171f34bd7773863c0f50
NitikaSakhunChaudhary <70484311+NitikaSakhunChaudhary@users.noreply.github.com>
2020-10-21 16:40:24 +0530

    Update sketch.js
    Amogh, Pls see these changes that I have made in the sketch.js. Read the comments to understand the changes. Make these changes and check. The project should work. I have only checked on my side. Its working now.

--- sketch.js
+++ sketch.js
@@ -16,24 +16,16 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
  	
-
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	//groundSprite.shapeColor("red")
-
-	wall1Sprite=createSprite(200,600,25,200);
-	//wall1Sprite.shapeColor("red")
-
-	wall2Sprite=createSprite(600,600,25,200);
-	//wall2Sprite.shapeColor("red")
-
+	groundSprite=createSprite(width/2, height-5, width,10);		//ground sprite
+	groundSprite.shapeColor="red";
 
 	engine = Engine.create();
 	world = engine.world;
@@ -44,37 +36,33 @@ function setup() {
 	World.add(world, packageBody);
 	
 
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	//Create a Ground- physical body for the ground
+	ground = Bodies.rectangle(width/2, height-5, width, 10 , {isStatic:true});	
  	World.add(world, ground);
 
-
 	Engine.run(engine);
-
-	
-
-	console.log(packageBody);
 }
 
-
+//groundsprite line 27 and physical body of the ground line 40 should have the same x, y positions 
 function draw() {
-  //rectMode(CENTER);
-  //rect(ground.position.x,ground.position.y,400,20);
+  rectMode(CENTER);
+  
   background(0);
   Engine.update(engine);
+  //package body and package  sprite should have the same x and y position
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  //ellipseMode(RADIUS);
-  //circle(packageBody.position.x,packageBody.position.y,20);
+  
   keyPressed(); 
   drawSprites();
- 
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-    Matter.Body.setStatic(packageBody,false);
+	 
+    //changed here you wrote Matter.Body.setStatic() where as namespacing is already done on the top as Body = Matter.Body;
+    Body.setStatic(packageBody,false);
 
   }
 }
